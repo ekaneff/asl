3bcc0b272692923aeff15f28075d9760dde2f77e
Emily Kaneff <emilykaneff@Emilys-MacBook-Pro.local>
2016-11-30 19:35:38 -0500

    routing lab from monday

---
+++ routing_lab/resources/views/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Routing lab | Home</title>
+</head>
+<body>
+
+	<header>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</header>
+
+	<section>
+		<h1>This is the Contact page!</h1>
+	</section>
+
+</body>
+</html>
---
+++ routing_lab/resources/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Routing lab | Home</title>
+</head>
+<body>
+
+	<header>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</header>
+
+	<section>
+		<h1>Welcome to the home page!</h1>
+	</section>
+
+</body>
+</html>
